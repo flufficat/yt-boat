ed002b3fb6238bfdd7702720ae5fd6e65229da4b
flufficat <flufficat.dev.synchronise@simplelogin.fr>
2022-09-05 20:53:31 +0100

    Updated install to include new commands

--- install.sh
+++ install.sh
@@ -15,7 +15,13 @@ sudo cp yt-boat /usr/local/bin
 
 # Adding macro commands to newsboat config
 echo -e "${y}Adding macro commands to newsboat config${r}"
-echo -e '\n# yt-boat macro\nmacro a set browser "yt-boat --add" ; open-in-browser ; set browser "exo-open --launch WebBrowser\nmacro d set browser "yt-boat --download" ; open-in-browser ; set browser "exo-open --launch WebBrowser"\nmacro c set browser "yt-boat --clear" ; open-in-browser ; set browser "exo-open --launch WebBrowser""' >> $XDG_CONFIG_HOME/newsboat/config
+echo -e '\n# yt-boat macros:\n\
+macro a set browser "yt-boat --add" ; open-in-browser ; set browser "exo-open --launch WebBrowser\n\
+macro d set browser "yt-boat --download" ; open-in-browser ; set browser "exo-open --launch WebBrowser"\n\
+macro c set browser "yt-boat --clear" ; open-in-browser ; set browser "exo-open --launch WebBrowser""\n\
+macro q set browser "yt-boat -nbq" ; open-in-browser ; set browser "exo-open --launch WebBrowser"\n\
+macro q set browser "yt-boat -nbq" ; open-in-browser ; set browser "exo-open --launch WebBrowser"\
+' >> $XDG_CONFIG_HOME/newsboat/config
 
 # Finished
 echo -e "${g}Finished setting up${r}"
