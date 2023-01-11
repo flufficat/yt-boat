5ad432b3d50e20940a98c1834d59cd6330d658e6
flufficat <flufficat.dev.synchronise@simplelogin.fr>
2023-01-11 20:33:00 +0000

    updated github page and --help

--- uninstall.sh
+++ uninstall.sh
@@ -6,22 +6,8 @@ g="\e[0;32m"
 
 # Making queue
 echo -e "${y}Removing queue${r}"
-rm -rf yt-boat
+rm -rf $XDG_DATA_HOME/yt-boat
 
-# Copying yt-boat to path
+# Removing yt-boat from path
 echo -e "${y}Copying yt-boat to path (requires root)${r}"
-sudo rm -f /usr/local/bin/yt-boat
-
-# Adding macro commands to newsboat config
-echo -e "${y}Adding macro commands to newsboat config${r}"
-echo -e '\n# yt-boat macros:\n\
-macro a set browser "yt-boat --add $1" ; open-in-browser ; set browser "exo-open --launch WebBrowser\n\
-macro d set browser "yt-boat --download" ; open-in-browser ; set browser "exo-open --launch WebBrowser"\n\
-macro c set browser "yt-boat --clear" ; open-in-browser ; set browser "exo-open --launch WebBrowser""\n\
-macro u set browser "yt-boat --update" ; open-in-browser ; set browser"exo-open --launch WebBrowser""\n\
-macro q set browser "yt-boat -qnb" ; open-in-browser ; set browser "exo-open --launch WebBrowser"\n\
-macro h set browser "yt-boat -hnb" ; open-in-browser ; set browser "exo-open --launch WebBrowser"\
-' >> $XDG_CONFIG_HOME/newsboat/config
-
-# Finished
-echo -e "${g}Finished setting up${r}"
+sudo rm -f /usr/local/bin/yt-boat && echo -e "${g}Finished removing${r}"
