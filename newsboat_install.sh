4f0fa244318cb2beb9b36ec99b6e130d1252a7a8
flufficat <112202351+flufficat@users.noreply.github.com>
2023-07-12 15:01:51 +0100

    flag backards compat

--- newsboat_install.sh
+++ newsboat_install.sh
@@ -8,11 +8,11 @@ g="\e[0;32m"
 touch $XDG_CONFIG_HOME/newsboat/yt-boat
 echo -e "${y}Adding macro commands to newsboat config${r}"
 echo -e '# yt-boat macros:\n
-macro a set browser "yt-boat --add $1" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
-macro l set browser "yt-boat --add-alternative $1" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
-macro d set browser "yt-boat --download" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
-macro c set browser "yt-boat --clear" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
-macro u set browser "yt-boat --update" ; open-in-browser ; set browser"exo-open --launch WebBrowser"
+macro a set browser "yt-boat add $1" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
+macro l set browser "yt-boat add-alternative $1" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
+macro d set browser "yt-boat download" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
+macro c set browser "yt-boat clear" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
+macro u set browser "yt-boat update" ; open-in-browser ; set browser"exo-open --launch WebBrowser"
 macro q set browser "yt-boat -qnb" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
 macro h set browser "yt-boat -hnb" ; open-in-browser ; set browser "exo-open --launch WebBrowser"
 ' >> $XDG_CONFIG_HOME/newsboat/yt-boat
