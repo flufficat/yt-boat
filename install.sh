b19fe98c8f3b2d813901c97f696898e890750fa3
flufficat <112202351+flufficat@users.noreply.github.com>
2023-08-07 11:56:23 +0100

    rename queue file

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ r="\e[0m"
 g="\e[0;32m"
 
 # Making queue
-if test ! -d $XDG_DATA_HOME/yt-boat && echo -e "${y}Making queue${r}" ; mkdir $XDG_DATA_HOME/yt-boat ; touch $XDG_DATA_HOME/yt-boat/newsboat.queue
+if test ! -d $XDG_DATA_HOME/yt-boat && echo -e "${y}Making queue${r}" ; mkdir $XDG_DATA_HOME/yt-boat ; touch $XDG_DATA_HOME/yt-boat/yt-boat.queue
 
 # Copying yt-boat to path
 echo -e "${y}Copying yt-boat to path${r}"
